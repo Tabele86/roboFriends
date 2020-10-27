48015ea5bdbb34a17810bb2121ed412f38e3a62c
Tokur86 <tabele86@gmail.com>
2020-10-27 09:10:51 -0500

    added React hooks, .envlocal file, and a simple hover tooltip

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,50 +1,48 @@
-// import { render } from '@testing-library/react';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: ''
-		};
-	}
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((response) => response.json())
-			.then((users) => this.setState({ robots: users }));
-	}
+function App() {
+	const [ robots, setRobots ] = useState([]);
+	const [ searchfield, setSearchfield ] = useState('');
+	const [ count, setCount ] = useState(0); // for demo purposes
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value });
-		// console.log(filterRobot);
+	useEffect(
+		() => {
+			fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((users) => {
+				setRobots(users);
+			});
+			console.log(count);
+		},
+		[ count ]
+	); // if you add count, only run if count changes.
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value);
 	};
 
-	render() {
-		const { robots, searchfield } = this.state;
-		const filterRobot = robots.filter((robot) => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		});
+	const filteredRobots = robots.filter((robot) => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	});
 
-		return !robots.length ? (
-			<h1>Loading</h1>
-		) : (
-			<div className="tc">
-				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<Scroll>
-					<ErrorBoundry>
-						<CardList robots={filterRobot} />
-					</ErrorBoundry>
-				</Scroll>
+	return !robots.length ? (
+		<h1>Loading</h1>
+	) : (
+		<div className="tc">
+			<h1 className="f1">RoboFriends</h1>
+			<div className="tooltip">
+				<button onClick={() => setCount(count + 1)}>Click Me!</button>
+				<span className="tooltiptext">This just counts in console log.</span>
 			</div>
-		);
-	}
+
+			<SearchBox searchChange={onSearchChange} />
+			<Scroll>
+				<CardList robots={filteredRobots} />
+			</Scroll>
+		</div>
+	);
 }
 
 export default App;
